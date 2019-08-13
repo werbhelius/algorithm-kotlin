564025f0696f13a287e1c345c303d0ba16c873bb
werb <werbhelius@gmail.com>
2019-08-13 17:17:55 +0800

    add longestCommonPrefix

---
+++ src/leetcode/_14_longestCommonPrefix/LongestCommonPrefix.kt
@@ -0,0 +1,25 @@
+package leetcode._14_longestCommonPrefix
+
+/**
+ * https://leetcode-cn.com/problems/longest-common-prefix/
+ */
+class Solution {
+    fun longestCommonPrefix(strs: Array<String>): String {
+
+        if (strs.isEmpty()) return ""
+
+        var prefix = strs[0]
+
+        for (i in 1 until strs.size) {
+
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length - 1)
+                if (prefix.isEmpty()) return ""
+            }
+
+        }
+
+        return prefix
+
+    }
+}
